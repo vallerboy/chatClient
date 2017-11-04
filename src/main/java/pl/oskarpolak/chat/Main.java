713faaf80d91de716ff6899c560244682294458c
Oskar <oskarbank@gmail.com>
2017-11-04 12:20:04 +0100

    asd

--- src/main/java/pl/oskarpolak/chat/Main.java
+++ src/main/java/pl/oskarpolak/chat/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainView.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
--- src/main/java/pl/oskarpolak/chat/controllers/MainController.java
+++ src/main/java/pl/oskarpolak/chat/controllers/MainController.java
@@ -1,6 +1,13 @@
 package pl.oskarpolak.chat.controllers;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import pl.oskarpolak.chat.models.SocketConnector;
 
 import java.net.URL;
@@ -9,10 +16,31 @@ import java.util.ResourceBundle;
 public class MainController implements Initializable{
 
 
+    @FXML
+    Button buttonSend;
+
+    @FXML
+    TextArea textMessages;
+
+    @FXML
+    TextField textWriteMessage;
+
     private SocketConnector socketConnector = SocketConnector.getInstance();
 
     public void initialize(URL location, ResourceBundle resources) {
+        clickEnterOnWriteMessage();
         socketConnector.connect();
         socketConnector.sendMessage("Ping");
+    }
+
+    private void clickEnterOnWriteMessage() {
+        textWriteMessage.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent event) {
+                if(event.getCode() == KeyCode.ENTER){
+                    socketConnector.sendMessage(textWriteMessage.getText());
+                    textWriteMessage.clear();
+                }
+            }
+        });
     }
 }
