d1ea7230199b2d8680fbf38afa624594dc852ad2
Oskar <oskarbank@gmail.com>
2017-11-04 11:57:52 +0100

    asd

--- src/main/java/pl/oskarpolak/chat/controllers/MainController.java
+++ src/main/java/pl/oskarpolak/chat/controllers/MainController.java
@@ -1,4 +1,18 @@
 package pl.oskarpolak.chat.controllers;
 
-public class MainController {
+import javafx.fxml.Initializable;
+import pl.oskarpolak.chat.models.SocketConnector;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable{
+
+
+    private SocketConnector socketConnector = SocketConnector.getInstance();
+
+    public void initialize(URL location, ResourceBundle resources) {
+        socketConnector.connect();
+        socketConnector.sendMessage("Ping");
+    }
 }
--- src/main/java/pl/oskarpolak/chat/models/SocketConnector.java
+++ src/main/java/pl/oskarpolak/chat/models/SocketConnector.java
@@ -1,8 +1,7 @@
 package pl.oskarpolak.chat.models;
 
-import javax.websocket.ClientEndpoint;
-import javax.websocket.ContainerProvider;
-import javax.websocket.WebSocketContainer;
+import javax.websocket.*;
+import java.io.IOException;
 import java.net.URI;
 
 @ClientEndpoint
@@ -13,6 +12,7 @@ public class SocketConnector {
     }
 
     private WebSocketContainer container;
+    private Session session;
 
     private SocketConnector() {
         container = ContainerProvider.getWebSocketContainer();
@@ -20,6 +20,34 @@ public class SocketConnector {
 
     public void connect() {
         URI uri = URI.create("ws://localhost:8080/chat");
-
+        try {
+            container.connectToServer(this, uri);
+        } catch (DeploymentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Połączono!");
     }
+
+    @OnOpen
+    public void onOpen(Session session){
+        this.session = session;
+        System.out.println("POLACZONO");
+    }
+
+    public void sendMessage(String message){
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String message){
+        System.out.println(message);
+    }
+
+
 }
